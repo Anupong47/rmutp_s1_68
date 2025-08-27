5e74fda51766c491b0862eaa96a03a075d4b6ec9
Anupong janjaroen <anupong.janjaroen@gmail.com>
2025-08-27 16:44:48 +0700

    update output schema

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,22 @@
 import { Hono } from "hono";
+//import { PrismaClient } from "../generated/prisma/client";
+import PrismaClient from "@prisma/client";                                                                                         
 
+
+const prisma = new PrismaClient();
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello World!"));
 
 app.get("/about", (c) => {
     return c.json ({
-        mwssage: "About Page"
+        mwssage: "Anupong Janjaroen"
     });
+});
+app.get("/profile",() => {
+    //logic
+    const profile = prisma.profile.findmany();
+    return profile;
 });
 
 export default app;
